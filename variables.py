f819d15305d0a18393fe66f99a06d8a9732adbd2
TymKey <brayan123fcruz@outlook.com>
2020-11-24 12:26:45 -0500

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,24 @@
+# Reutilizar en memoria
+# Variable pueden tener cualquier nombre, las mayusculas afectan, case sensitive
+# Las variables tienen reglas Ejm: _1Dato, Dato, dato.
+name = "TymKey"
+nombre = "TymKey"
+Nombre = "TymKey"
+print(name)
+
+# Asignar variables en la misma linea
+x, apellido = 200, "Hades"
+print(x)
+print(apellido) # Imprimir en la misma linea
+print(x, apellido)
+
+# conventiones
+
+note_name = "Code" # Snake case
+noteName = "Study Code" # Camel case
+NoteName = "Study Data" # Pascal case
+
+# Todo en mayuscula por que es un valor que no cambia, python.
+
+PI = 3.1416
+MY_NAME = "TymKey"
