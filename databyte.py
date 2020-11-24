3bbaadfd3faed093c36f9599f8d8dea5ff68937d
TymKey <brayan123fcruz@outlook.com>
2020-11-24 12:02:07 -0500

    Create databyte.py

---
+++ databyte.py
@@ -0,0 +1,40 @@
+# Strings, se puede usar triple comillas.
+# Funciones
+print("Hello World")
+print('Hello World')
+
+# información del texto y imprime esa información.
+print(type("Hello World")) 
+
+# concatenar cosas, uniendo algo Ejm: ByeWorld sin espacio. 
+print("Bye" + "World")
+
+# Numbers:
+
+# Entero, Integer
+print(30)
+
+# Decimal, Float
+print(30.5)
+
+# Datos logicos Boolean, bool
+# True and False
+
+# List
+[10, 20, 30, 55]
+['Hello', 'Bye', 'Lost']
+[10, "Hello", True, 10.1]
+
+# Tuples, Datos que no se cambian, inmutable, tuple
+(10, 20, 30, 55)
+
+# Dictorionies, dict
+{   
+    "Key And Value"
+    "name": "Camilo",
+    "apellido": "Cruz",
+    "apodo": "Gorda"
+}
+
+# No esta definido
+None
